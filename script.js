0b6e2768a038ec33321c35f7af2adb097f21d8c3
niirmaaltwaatii <twaatiiniirmaal@gmail.com>
2020-07-27 12:21:26 +0545

    some changes added

--- script.js
+++ script.js
@@ -2,15 +2,46 @@
 JavaScript for ToDoJS
 Source-Code : https://www.github.com/niirmaaltwaatii/ToDoJS.git
 */
-function add() {
+var id = 0;
+
+var arr = [
+
+];
+
+function add(){
+  add0(id);
+}
+
+function add0(id) {
+  id += 1;
+  this.id = id;
+  console.log(id);
+  console.log(this.id);
   var text = document.getElementById("text");
   var list = document.getElementById("list");
 
   var li = document.createElement("li");
-  var txt = document.createTextNode(text.value);
+  li.id=this.id;
 
-  li.appendChild(txt);
+  var span = document.createElement("span");
+  var txt = document.createTextNode(text.value);
+  span.appendChild(txt);
+
+  var chk =document.createElement("input");
+  chk.type="checkbox";
+
+  var btn = document.createElement("button");
+  btn.className="del";
+  btn.innerHTML="DELETE";
+  // btn.onclick=document.getElementById(this.id).visibility="hidden";
+
+  li.appendChild(chk);
+  li.appendChild(span);
+  li.appendChild(btn);
   list.appendChild(li);
+
+  // arr.push(li);
+  // list.appendChild(arr.map());
 
   list.style.visibility = "visible";
   text.value = "";
